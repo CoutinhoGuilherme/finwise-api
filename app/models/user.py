c811f3c76310840ebc8e8f083d1d1426853959df
Guilherme Coutinho de Moura <69174628+CoutinhoGuilherme@users.noreply.github.com>
2025-06-05 00:52:46 +0000

    fix:models/uers.py

--- app/models/user.py
+++ app/models/user.py
@@ -15,4 +15,4 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    transactions = relationship("Transaction", back_populates="user", cascade="all, delete-orphan"
+    transactions = relationship("Transaction", back_populates="user", cascade="all, delete-orphan")
